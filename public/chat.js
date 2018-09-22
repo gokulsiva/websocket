f8018b678ef4ccd7d77acfaf783911477797ce28
gokulsiva <gsgokul305@gmail.com>
2018-09-22 15:55:38 +0530

    Initial commit.

---
+++ public/chat.js
@@ -0,0 +1,3 @@
+//Make connection to the socket
+
+let socket = io.connect('http://localhost:4000');
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import * as express from 'express'
+import * as socket from 'socket.io'
+
+//App Setup
+
+let app = express.default();
+let server = app.listen(4000, () => {
+    console.log('Server started on port 4000.');
+});
+
+//Static files
+app.use(express.static('public'));
+
+//Socket setup
+let io = socket.default(server);
+
+io.on('connection', (socket) => {
+    console.log('**** Socket connection is made to the server ****', socket.id);
+});
